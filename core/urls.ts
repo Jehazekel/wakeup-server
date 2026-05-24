57b63e459cbbbdcae0df645c53901c7d5953d0d7
Jehazekel <jeremiahstrong321@gmail.com>
2026-05-24 02:04:32 +0000

    added n8n hosted url

--- core/urls.ts
+++ core/urls.ts
@@ -1,3 +1,4 @@
 export const HOSTED_URLS: string[] = [
-    'https://whatsapp-chatbot-zev3.onrender.com'
+    'https://n8n-latest-bf9i.onrender.com'
+    // 'https://whatsapp-chatbot-zev3.onrender.com',
 ];
